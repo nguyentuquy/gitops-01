0b2e66170d11cb8b951e7ac10c484225a94dc67c
quynt <tu.quy.nguyen@conuti.de>
2024-03-16 19:47:41 +0100

    test on mac

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello Quy test auto deploy 4444
+          Hello Quy test auto deploy 55
         </a>
       </header>
     </div>
